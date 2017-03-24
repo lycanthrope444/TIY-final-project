bbaa954476e9fbf1e3cbb1546ad6733e4c69d796
Nathan <nathanstarwalt@gmail.com>
2017-03-24 10:11:42 -0400

    publish: https for proxy server
    generated from commit 044a233626817a426a679f080039df2e8e58ac94

--- js/bundle.js
+++ js/bundle.js
@@ -2182,9 +2182,9 @@ var $ = require('jquery');
 // Info to connect to the proxy server
 // Proxy server is used to secure the apikey data
 var proxy = {
-  // PROXY_API_URL:'https://lycanthrope-proxy.herokuapp.com/',
+  PROXY_API_URL:'https://lycanthrope-proxy.herokuapp.com/',
   //info for localhost proxy
-  PROXY_API_URL:'http://localhost:3000/'
+  // PROXY_API_URL:'http://localhost:3000/'
 };
 
 module.exports = proxy;
